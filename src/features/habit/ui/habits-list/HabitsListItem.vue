6ec02b68c4cc8bcc7880b930820a565254fb8f72
andreyDanilenko <danilenko.a.g@mail.ru>
2026-01-20 16:13:07 +0300

    add(icon): add new icon component in habitsListItem

--- src/features/habit/ui/habits-list/HabitsListItem.vue
+++ src/features/habit/ui/habits-list/HabitsListItem.vue
@@ -22,28 +22,14 @@
             @click.stop="$emit('mark-completion', habit)"
             title="Отметить выполнение"
           >
-            <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 13l4 4L19 7"
-              />
-            </svg>
+            <CheckIcon size="xs"/>
           </button>
           <button
             class="p-1 text-gray-400 hover:text-red-600"
             @click.stop="$emit('delete', habit)"
             title="Удалить привычку"
           >
-            <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-              />
-            </svg>
+            <DeleteIcon size="xs"/>
           </button>
         </div>
       </div>
@@ -77,6 +63,8 @@
 <script setup lang="ts">
   import { ProgressBar } from '@/shared/ui'
   import type { Habit } from '@/entities/habit'
+  import { CheckIcon, DeleteIcon } from '@/shared/ui/icon'
+  3
 
   defineProps<{
     habit: Habit
