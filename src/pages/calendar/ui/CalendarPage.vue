aa4d10b6cf16049988404f7ed6f81e01424d08c8
andreyDanilenko <danilenko.a.g@mail.ru>
2026-01-25 19:32:05 +0300

    feature(calendar): add calendar page

---
+++ src/pages/calendar/ui/CalendarPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="max-w-7xl mx-auto space-y-6">
+    <CalendarPageHeader
+      :formatted-month="formattedMonth"
+      @prev-month="prevMonth"
+      @next-month="nextMonth"
+    />
+
+    <CalendarLegend />
+
+    <CalendarGrid
+      :calendar-days="calendarDays"
+      :week-days="weekDays"
+      :is-loading="isLoading"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { CalendarPageHeader, CalendarLegend, CalendarGrid } from '@/features/calendar'
+  import { useCalendarPage } from '@/features/calendar/model'
+
+  const {
+    calendarDays,
+    formattedMonth,
+    weekDays,
+    isLoading,
+    prevMonth,
+    nextMonth,
+  } = useCalendarPage()
+</script>
