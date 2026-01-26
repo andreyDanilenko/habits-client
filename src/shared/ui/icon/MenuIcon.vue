ec143cf10000ab8b5af21cae9c694e13d3d26f76
andreyDanilenko <danilenko.a.g@mail.ru>
2026-01-26 03:35:28 +0300

    add(icons): add new icons

---
+++ src/shared/ui/icon/MenuIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <BaseIcon v-bind="$attrs">
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    />
+  </BaseIcon>
+</template>
+
+<script setup lang="ts">
+  import BaseIcon from './BaseIcon.vue'
+</script>
