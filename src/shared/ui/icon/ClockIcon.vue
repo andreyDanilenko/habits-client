3c41549d21570d05292e64ba52d21b18115f12b8
andreyDanilenko <danilenko.a.g@mail.ru>
2026-03-19 20:13:27 +0300

    feat(ui): add Clock, Play, Stop icons

---
+++ src/shared/ui/icon/ClockIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <BaseIcon v-bind="$attrs">
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </BaseIcon>
+</template>
+
+<script setup lang="ts">
+  import BaseIcon from './BaseIcon.vue'
+</script>
---
+++ src/shared/ui/icon/PlayIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <BaseIcon v-bind="$attrs">
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M5.25 5.653c0-.856.932-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.986V5.653z"
+    />
+  </BaseIcon>
+</template>
+
+<script setup lang="ts">
+  import BaseIcon from './BaseIcon.vue'
+</script>
